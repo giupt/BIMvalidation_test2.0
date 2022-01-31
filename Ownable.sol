ffc767d4fa211e1ebecf6de160f3c3db6731752a
giupt <86053202+giupt@users.noreply.github.com>
2022-01-31 14:16:18 +0000

    Update Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -3,13 +3,15 @@ pragma solidity ^0.8.0;
 //@title Ownable
 //@dev The Ownable contract has an owner address, and provides basic authorization control
 //functions, this simplifies the implementation of "user permissions"
+
 contract Ownable {
     address public owner;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
 //@dev The Ownable constructor sets the original `owner` of the contract to the sender
-    constructor() {
+
+    constructor() public {
         owner = msg.sender;
     }
 
